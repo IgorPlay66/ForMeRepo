ee2f810d2d922e5003fa7442234036a59dbc01ab
FreezyCat <73362398+IgorPlay66@users.noreply.github.com>
2025-03-05 00:00:31 +0500

    Update open_ai_helper.py

--- web_gpt/open_ai_helper.py
+++ web_gpt/open_ai_helper.py
@@ -1,31 +1,34 @@
 import os
 import openai
-
+import asyncio
+import json
+import re
 
 class OpenAiHelper:
     def __init__(self, model: str):
         self.model = model
         self.functions = [
-                {
-                    "name": "internet_search",
-                    "description": "Функция для нахождения информации в интернете, ее надо вызывать когда для ответа юзера требуется актуальная информация из интернета",
-                    "parameters": {
-                        "type": "object",
-                        "properties": {
-                            "query": {
-                                "type": "string",
-                                "description": "Запрос в интернет, если в запросе есть ссылка, то передавай сообщение юзера БЕЗ изменений"
-                            }
-                        },
-                        "required": ["properties"],
+            {
+                "name": "internet_search",
+                "description": "Функция для нахождения информации в интернете, ее надо вызывать когда для ответа юзера требуется актуальная информация из интернета",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "query": {
+                            "type": "string",
+                            "description": "Запрос в интернет, если в запросе есть ссылка, то передавай сообщение юзера БЕЗ изменений"
+                        }
                     },
-                }
-            ]
+                    "required": ["properties"],
+                },
+            }
+        ]
 
-    async def chat_completion(self, messages):
-        openai.api_key = self.get_open_ai_key()
+    async def chat_completion(self, messages, endpoint=None, key=None, model=None):
+        openai.api_key = key if key else self.get_open_ai_key()
+        openai.api_base = endpoint if endpoint else "https://api.openai.com/v1"
         return await openai.ChatCompletion.acreate(
-            model=self.model,
+            model=model if model else self.model,
             messages=messages,
             functions=self.functions
         )
